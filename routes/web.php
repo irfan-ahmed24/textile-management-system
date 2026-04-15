b4930e5cd0cc3e84d6577d0c9615b682a5708f94
irfan-ahmed <irfanahmed99989@gmail.com>
2026-04-16 01:41:30 +0600

    admin login fully done

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,18 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        $user = Auth::user();
+        if ($user->role === 'Admin') {
+            return redirect()->intended('/admin/dashboard');
+        } elseif ($user->role === 'Inventory Manager') {
+            return redirect()->intended('/inventory/dashboard');
+        } elseif ($user->role === 'Production Manager') {
+            return redirect()->intended('/production/dashboard');
+        } else {
+            return redirect()->intended('/buyer/dashboard');
+        }
+    }
     return Inertia::render('Welcome');
 })->name('home');
 
